f5ab038d28820b15fdd0bcf467ec31b6c5f3504c
Curtis Kularski <curtis@kularski.org>
2025-03-21 20:00:26 -0400

    Added ExifNotes support

---
+++ ExifNotes.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExIF35
+{
+
+    public class ExifNotesRoll
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string unloaded { get; set; }
+        public enCamera camera { get; set; }
+        public int iso { get; set; }
+        public string format { get; set; }
+        public bool archived { get; set; }
+        public Filmstock filmStock { get; set; }
+        public bool favorite { get; set; }
+        public Frame[] frames { get; set; }
+    }
+
+    public class enCamera
+    {
+        public int id { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public string minShutter { get; set; }
+        public string maxShutter { get; set; }
+        public string shutterIncrements { get; set; }
+        public string exposureCompIncrements { get; set; }
+        public string format { get; set; }
+    }
+
+    public class Filmstock
+    {
+        public int id { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public int iso { get; set; }
+        public string type { get; set; }
+        public string process { get; set; }
+        public bool isPreadded { get; set; }
+    }
+
+    public class Frame
+    {
+        public int id { get; set; }
+        public int count { get; set; }
+        public string date { get; set; }
+        public string shutter { get; set; }
+        public string aperture { get; set; }
+        public Location location { get; set; }
+        public string formattedAddress { get; set; }
+        public int focalLength { get; set; }
+        public int noOfExposures { get; set; }
+        public bool flashUsed { get; set; }
+        public string lightSource { get; set; }
+        public enLens lens { get; set; }
+        public string exposureComp { get; set; }
+        public Filter[] filters { get; set; }
+        public string description {  get; set; }
+    }
+
+    public class Location
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+    }
+
+    public class enLens
+    {
+        public int id { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public string serialNumber { get; set; }
+        public string minAperture { get; set; }
+        public string maxAperture { get; set; }
+        public int minFocalLength { get; set; }
+        public int maxFocalLength { get; set; }
+        public string apertureIncrements { get; set; }
+    }
+
+    public class Filter
+    {
+        public int id { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+    }
+
+
+}
---
+++ ImportExifNotes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+using System.Diagnostics;
+
+namespace ExIF35
+{
+    class ImportExifNotes
+    {
+        public static Roll importRoll(String filepath, String photographer)
+        {
+            Roll newRoll = new Roll();
+
+            string jsonString = File.ReadAllText(filepath);
+            ExifNotesRoll theRoll = JsonSerializer.Deserialize<ExifNotesRoll>(jsonString);
+
+            newRoll.cameramake = theRoll.camera.make;
+            newRoll.cameramodel = theRoll.camera.model;
+            newRoll.FilmType = theRoll.filmStock.make + " " + theRoll.filmStock.model;
+            newRoll.loadDate = theRoll.date.Replace('-', ':').Replace('T', ' ');
+
+            if (theRoll.unloaded != null)
+            {
+                newRoll.unloadDate = theRoll.unloaded.Replace('-', ':').Replace('T', ' ');
+            }
+
+            String cameraSerialNumber = "";
+            String loadDate = "";
+            String unloadDate = ""; 
+
+            //create objects to hold exposure IDs and content for various attributes
+            Dictionary<int, int> flash = new Dictionary<int, int>();
+            Dictionary<int, String> description = new Dictionary<int, String>();
+            Dictionary<int, String> expNum = new Dictionary<int, String>();
+            Dictionary<int, String> comp = new Dictionary<int, String>();
+            Dictionary<int, String> focallength = new Dictionary<int, String>();
+            Dictionary<int, String> aperture = new Dictionary<int, String>();
+            Dictionary<int, String> shutter = new Dictionary<int, String>();
+            Dictionary<int, String> time = new Dictionary<int, String>();
+            Dictionary<int, int> filterID = new Dictionary<int, int>();
+            Dictionary<int, int> lensID = new Dictionary<int, int>();
+            Dictionary<int, int> gpsID = new Dictionary<int, int>();
+            Dictionary<int, int> light = new Dictionary<int, int>();
+
+            Dictionary<int, String> filters = new Dictionary<int, String>();
+            Dictionary<int, String> lenses = new Dictionary<int, String>();
+            Dictionary<int, Double> longitudes = new Dictionary<int, double>();
+            Dictionary<int, Double> latitudes = new Dictionary<int, double>();
+            Dictionary<int, Double> altitudes = new Dictionary<int, double>();
+
+            foreach (Frame fr in theRoll.frames)
+            {
+                int usedflash = 0; 
+                if (fr.flashUsed)
+                {
+                    usedflash = 1; 
+                }
+                int lightSource = 0;
+                switch (fr.lightSource)
+                {
+                    case "Unknown": lightSource = 0; break;
+                    case "Daylight": lightSource = 1; break;
+                    case "Shade": lightSource =  7; break;
+                    case "Cloudy": lightSource = 6; break;
+                    case "Fluorescent": lightSource = 2; break;
+                    case "Tungsten": lightSource = 3; break;
+                    case "Flash": lightSource = 4; break;
+                    case "Sunny": lightSource = 9; break;
+                }
+
+                String usedfilters = "";
+                int filteri = 0;
+                if (fr.filters != null)
+                {
+                    foreach (Filter tFilter in fr.filters)
+                    {
+                        if (filteri > 0)
+                        {
+                            usedfilters += ", ";
+                        }
+                        usedfilters += tFilter.model;
+                        filteri++;
+                    }
+                }
+
+
+                //String Number, String Aperture, String Speed, String Date, String DateDigitzed, String Title, String Descriprtion, String Photographer, String FL
+                Exposure tempExp = new Exposure(fr.count.ToString(), fr.aperture, fr.shutter, fr.date.Replace('-', ':').Replace('T', ' '), "", "", fr.description, photographer, fr.focalLength.ToString());
+                tempExp.addExtended(0, 0, usedflash, 0, lightSource, fr.exposureComp, 0, 0, usedfilters);
+                tempExp.addLensInfo(fr.lens.model);
+                tempExp.addGPS(fr.location.latitude, fr.location.longitude, 0);
+                newRoll.add(tempExp);
+
+            }
+
+            return newRoll;
+
+        }
+    }
+}
